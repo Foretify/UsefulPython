2d5972542b14647946c42b0d4492224d41d1b68c
Lyle Wright <lyle@foretify.ai>
2022-10-11 13:44:11 -0700

    Created our pseudo code

--- esri_system_health_check/src/esri_system_health_check/main.py
+++ esri_system_health_check/src/esri_system_health_check/main.py
@@ -12,10 +12,12 @@ def main():
     print(runner_start_time)
 
 
-    # Read Configuration File
-    # This is the configuration file that has all connection info for the script to 
-    # run ie layers from AGOL and connection parameters
-    # Create the config parser object to store all the connection information for AGOL
+    # 1. Can we connect to the ArcGIS Portal or AGOL @topowright
+
+        # Read Configuration File
+        # This is the configuration file that has all connection info for the script to 
+        # run ie layers from AGOL and connection parameters
+        # Create the config parser object to store all the connection information for AGOL
 
     print("Reading details Configuration File")
     
@@ -23,17 +25,27 @@ def main():
     
     config = common.load_configuration(config_dir)
     print(f"configuration file location:\n{config_dir}")
-    
-   
-
-
-    
     print("Querying Content from ArcGIS")
     arcgis_org_url = config.get('ArcGIS Credentials', 'arcgis_org_url')
     username = config.get('ArcGIS Credentials', 'username')
     password = config.get('ArcGIS Credentials', 'password')
 
     gis = common.connect(org_url=arcgis_org_url, login_name=username, user_password=password)
+
+
+    # 2. Can we connect to ArcGIS Server
+    # 3. Can we connect to the web adaptors for portal and server
+    # 4. We need to understand the user type and permissions of the user
+    # 5. We need to be able to create a folder and delete a folder in the users content
+    # 6. We need to be able to publish a shapefile as a hosted feature service
+    # 7. We need to be able to publish an excel as a hosted feature service
+    # 8. We need to be able to pull down these files and edit the content and push back our edits
+    # 9. We need to be able to run a geometry query on this data and produce a new layer in the enterprise based on this query
+    # 10. We need to be able to create new users
+    
+
+
+    
 
     print("Completed all necessary steps")
 
