adcd92266af3bdeeb5deb1fe971e3d9166b9f5d9
Lyle Wright <lyle@foretify.ai>
2022-11-08 15:28:22 -0800

    Update main.py

--- h3/main.py
+++ h3/main.py
@@ -7,18 +7,18 @@ import time
 def main():
     runner_start_time = time.time()
     
-    hex_dictionary = cm.get_hex_grid_info(28.32,55.46,7)
+    hex_dictionary = cm.get_hex_grid_info(34.82,-116.46,7)
     print(hex_dictionary)
     print("##########################################################\n")
     
-    hex_grid = cm.create_hex_grid(25.32,55.46,7,3)
+    hex_grid = cm.create_hex_grid(35.02,-116.36,8,10)
     #print(len(hex_grid))
     #print(hex_grid.head()
     print("##########################################################\n")
-    cm.create_shapefile(hex_grid,"C:\Working","final.shp")
+    cm.create_shapefile(hex_grid,"C:\Working","Cali1.shp")
     print("##########################################################\n")
     
-    cm.create_hex_csv(hex_grid,"C:\Working","final.csv" )
+    cm.create_hex_csv(hex_grid,"C:\Working","Cali1.csv" )
     print("##########################################################\n")
     
     
