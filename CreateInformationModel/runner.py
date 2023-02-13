48c9a1a6c2e6c0821c457d8cc276a4aa5e3263b3
Lyle Wright <lyle@foretify.ai>
2023-02-13 13:45:05 -0800

    projection file for script

---
+++ CreateInformationModel/runner.py
@@ -0,0 +1,18 @@
+# Name: CreateFeatureDataset_Example2.py
+# Description: Create a feature dataset 
+
+# Import system modules
+import arcpy
+
+""" # Set local variables
+out_dataset_path = "C:/output/HabitatAnalysis.gdb" 
+out_name = "analysisresults"
+
+# Create a spatial reference object
+sr = arcpy.SpatialReference("C:/data/studyarea.prj")
+
+# Create a FileGDB for the fds
+arcpy.CreateFileGDB_management("C:/output", "HabitatAnalysis.gdb")
+
+# Execute CreateFeaturedataset 
+arcpy.CreateFeatureDataset_management(out_dataset_path, out_name, sr) """
