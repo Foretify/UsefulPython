88e3951f6f2cccc78eaa1fe137f36275acd28c7c
Lyle Wright <lyle@foretify.ai>
2023-02-14 05:50:11 -0800

    updates to the schema

--- CreateInformationModel/main.py
+++ CreateInformationModel/main.py
@@ -15,13 +15,28 @@ else:
     print('Creating the geo database')
     arcpy.CreateFileGDB_management(folder_path, geo_database_name)
 
-im_dict = { 'hpt_targets'   : ['targets',"point","HPT"],
-                      'hvt_targets'   : ['targets',"point","HVT"],
-                      'known_entity'   : ['entity',"point","Known"],
-                      'unknown_entity'   : ['entity',"point","Unknown"],
-                      'point_gmti'   : ['gmti',"point","Point"],
-                      'track_gmti'   : ['gmti',"point","Track"],
-                      'to_gmti'   : ['gmti',"polygon","Tessellated Overlay"],
+im_dict = { 'hpt_targets'   : ['targets',"POINT","HPT"],
+            'hvt_targets'   : ['targets',"POINT","HVT"],
+            'known_entity'   : ['entity',"POINT","Known"],
+            'unknown_entity'   : ['entity',"POINT","Unknown"],
+            'point_gmti'   : ['gmti',"POINT","Point"],
+            'track_gmti'   : ['gmti',"POINT","Track"],
+            'to_gmti'   : ['gmti',"POLYGON","Tessellated Overlay"],
+            'ellipse'   : ['elint',"POLYGON"," Ellipse"],
+            'convolved_point_elint' : ['elint','POLYGON','Convolved Points'],
+            'overlay_polygon_elint' : ['elint','POLYGON','Overlay Polygons'],
+            'point_bda'   : ['bda',"POINT","BDA Point"],
+            'line_bda'   : ['bda',"POLYLINE","BDA Line"],
+            'polygon_bda'   : ['bda',"POLYGON","BDA Polygon"],
+            'units_ops_graphics'   : ['ops_graphics',"POINT","Units"],
+            'points_ops_graphics'   : ['ops_graphics',"POINT","Points"],
+            'lines_ops_graphics'   : ['ops_graphics',"POLYLINE","Lines"],
+            'polygons_ops_graphics'   : ['ops_graphics',"POLYGON","Polygons"],
+            'units_intel_graphics'   : ['intel_graphics',"POINT","Units"],
+            'points_intel_graphics'   : ['intel_graphics',"POINT","Points"],
+            'lines_intel_graphics'   : ['intel_graphics',"POLYLINE","Lines"],
+            'polygons_intel_graphics'   : ['intel_graphics',"POLYGON","Polygons"],
+            'spot_reports'   : ['reports',"POINT","BDA Line"],
 
 }
 for key, value in im_dict.items():
